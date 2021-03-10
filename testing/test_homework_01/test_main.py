ce2739792e4d42115644b943b3bcfa84535f81e4
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-03-10 17:16:24 +0300

    fix tests for odd/even

--- testing/test_homework_01/test_main.py
+++ testing/test_homework_01/test_main.py
@@ -34,12 +34,22 @@ def ip (O000OO0O0O000OOO0 )->bool :
 @pytest .fixture
 def f (request ):
     OO0O0O0O0OO000000 =request .param
-    OO00O00OOOO000OOO ={homework .ODD :lambda O0OO0O00O0OOO00OO :O0OO0O00O0OOO00OO %2 ==0 ,homework .EVEN :lambda OO0000OO0OO00OO00 :OO0000OO0OO00OO00 %2 !=0 ,homework .PRIME :ip ,}
+    OO00O00OOOO000OOO ={homework .ODD :lambda O0OO0O00O0OOO00OO :O0OO0O00O0OOO00OO %2 !=0 ,homework .EVEN :lambda OO0000OO0OO00OO00 :OO0000OO0OO00OO00 %2 ==0 ,homework .PRIME :ip ,}
     OO000O0O00OOO0O0O =fake .pylist (nb_elements =fake .pyint (30 ,100 ),value_types =["int"])
     OOOOOO0O0O0OO0O00 =list (filter (OO00O00OOOO000OOO [OO0O0O0O0OO000000 ],OO000O0O00OOO0O0O ))
     return OO0O0O0O0OO000000 ,OO000O0O00OOO0O0O ,OOOOOO0O0O0OO0O00
 @pytest .mark .parametrize ("f",[homework .ODD ,homework .EVEN ,homework .PRIME ],indirect =True )
 def test_filter_numbers (f, m ):
     OO0OOOOOO0O0OOOO0 ,OO0O0O000OO00OO0O ,OO000OO0OO0OOOO0O =f
+    OOOO0O00O0O0O0000 =homework .filter_numbers (OO0O0O000OO00OO0O ,OO0OOOOOO0O0OOOO0 )
+    assert OOOO0O00O0O0O0000 ==OO000OO0OO0OOOO0O
+
+@pytest .mark .parametrize ("p", [
+    (homework.ODD, [1,2,3,4,5,6,7,8,9], [1,3,5,7,9]),
+    (homework.EVEN, [2,3,4,5,6,7,8,9,10], [2,4,6,8,10]),
+    (homework.PRIME, [3,4,5,6,7,8,9,11], [3,5,7,11]),
+])
+def test_filter_numbers_consts(p):
+    OO0OOOOOO0O0OOOO0 ,OO0O0O000OO00OO0O ,OO000OO0OO0OOOO0O =p
     OOOO0O00O0O0O0000 =homework .filter_numbers (OO0O0O000OO00OO0O ,OO0OOOOOO0O0OOOO0 )
     assert OOOO0O00O0O0O0000 ==OO000OO0OO0OOOO0O
