f1854479ec308b98ae78b30364a1fbc358f2ca02
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-05-26 21:57:52 +0300

    Add lesson 1: Intro

---
+++ lessons/lesson.01/main.py
@@ -0,0 +1,19 @@
+print("Hello world!")
+print('Hello world!')
+
+print("""Hello
+world
+!!!
+""")
+
+
+"""
+[ "ab", "cd", "de", "ef"]
+    0 |   1 |   2 |   3 |
+  -4  | -3  | -2  | -1
+"""
+"""
+['abc', 'qwe', '123', 'foo', 'bar']
+    0 |    1 |    2 |    3 |    4 |
+| -5  | -4   | -3   | -2   | -1
+"""
