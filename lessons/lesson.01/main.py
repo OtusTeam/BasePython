f7acad61ec1605ede85bcc19d0ce9e72d97295d1
Suren Khorenyan <surenkhorenyan@gmail.com>
2022-08-30 21:53:44 +0300

    Add lesson 01: intro

---
+++ lessons/lesson.01/main.py
@@ -0,0 +1,19 @@
+"""
+Зачем этот файл?
+Это наш первый файл
+
+lower_snake_case -- имя файлов .py
+"""
+
+'''
+точно также
+'''
+
+print("Hello World!")
+print('Hello World!')
+
+1 + 2
+3 + 4
+
+print("hello")
+print(5 + 7)
