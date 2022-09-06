9f95ed6bf8281ba033d3c3d0362af0fd64b3a16f
Suren Khorenyan <surenkhorenyan@gmail.com>
2022-09-06 21:43:24 +0300

    Add lesson 2: funcs and data types

---
+++ lessons/lesson.02/main.py
@@ -0,0 +1,16 @@
+evens = (2, 4, 6)
+# evens.__add__()
+
+more_evens = evens + (8, 10)
+
+print(evens)
+print(more_evens)
+
+print(print.__doc__)
+print(print.__name__)
+# print.__dir__()
+evens.count(8)
+
+
+# attrs
+# methods
