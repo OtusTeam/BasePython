8c76a49e820a3b0157a8da6c7eae6705c00dab24
jo <jo@local>
2022-11-29 23:55:15 +0500

    Add lesson 24: bootstrap

---
+++ lessons/lesson.24/static/js/product-details.js
@@ -0,0 +1,14 @@
+function sendDeleteRequest(url, onSuccess) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("DELETE", url, true);
+    xhr.onload = function () {
+        const data = JSON.parse(xhr.responseText);
+        if (xhr.readyState == 4 && xhr.status == "200") {
+            onSuccess(data)
+        } else {
+            console.error(data);
+        }
+    }
+    xhr.send(null);
+}
---
+++ lessons/lesson.24/views/forms/products.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+
+
+class CreateProductForm(FlaskForm):
+    name = StringField(
+        label="Product name",
+        name="product-name",
+        validators=[
+            DataRequired(),
+            Length(min=3),
+        ],
+        # render_kw={'class': 'form-control'}
+    )
