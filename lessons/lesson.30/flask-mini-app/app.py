66e8ad567ff70ba57afdb5f4c00903663d546899
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-07-18 21:38:23 +0300

    Add lesson 30: Flask intro

---
+++ lessons/lesson.30/flask-mini-app/app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+from views.products import products_app
+
+app = Flask(__name__)
+app.register_blueprint(products_app)
+
+
+@app.route("/")
+def hello():
+    return "<h1>Hello World!</h1>"
+
+
+@app.route("/hello/")
+def hello_optional_name():
+    name = request.args.get("name") or ""
+    # return f"<h1>Hello {name}</h1>"
+    name = name.strip()
+    if not name:
+        name = "World"
+    return {
+        "name": name,
+        "ids": request.args.getlist("id"),
+    }
+
+
+@app.route("/hello/<name>/")
+def hello_name(name):
+    # return f"<h1>Hello {name}</h1>"
+    name = name.strip()
+    return render_template(
+        "index.html",
+        name=name,
+    )
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host="0.0.0.0",
+        port=5050,
+    )
---
+++ lessons/lesson.30/flask-mini-app/views/products/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ("products_app",)
+
+from .views import products_app
