e52913e40cc13e059e2f284e73a24352bf2e16a3
Alexandr Ignatev <136903931+Cur1yB@users.noreply.github.com>
2024-09-10 13:43:50 +0700

    Add lesson 25: Relation many-to-many

---
+++ lessons/lesson.25/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models import Base
+
+engine = create_engine("sqlite:///vet_clinic.db", echo=False)
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
+
+
+def create_all():
+    Base.metadata.create_all(engine)
---
+++ lessons/lesson.25/main.py
@@ -0,0 +1,4 @@
+from database import create_all
+
+if __name__ == "__main__":
+    create_all()
