2c73f7c263bf24965accc259ab47d12812004a09
Stanislav-Wise <111073899+Stanislav-Wise@users.noreply.github.com>
2024-10-24 18:50:14 +0300

    Delete directory

--- lesson.41/blogapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlogappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blogapp'
--- lesson.41/blogapp/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name='upper_name')
-def upper_name(value):
-    return value.upper()
