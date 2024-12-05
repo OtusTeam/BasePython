fdcea26903bd1d51e2bbcec286de717a1357b928
STONE <17th@inbox.ru>
2024-12-05 23:35:24 +0300

    add_lesson.02

---
+++ lessons/lesson.02/main.py
@@ -0,0 +1,138 @@
+# a = 5
+# b = a
+# print(a)
+# print(b)
+# a = a + 1
+# # a += 1
+# print(a)
+# print(b)
+
+# int
+
+# a = 10
+# # print(a)
+# # print(type(a))
+# b = 3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
+# print(-a)
+
+# float
+
+# a = 5.7
+# print(a)
+
+# c = 0.57
+# print(c * 10)
+
+# Decimal
+
+# print(0.1 + 0.1 + 0.1 == 0.3)
+# print(0.1 + 0.1 == 0.2)
+
+# t = 2
+# d = 4.5546568697
+# print(round(d, t))
+
+import math
+
+# k = 5.02
+# print(math.ceil(k))
+# print(int(k) + 1)
+# l = 5.89
+# print(math.floor(l))
+# # print(int(l))
+
+# print(5 == 5.0)
+
+# str
+
+# a = "kjgld'kfjg"
+# print(a)
+# print(type(a))
+
+# first = 'otus'
+# second = 'python'
+# cof = 10
+# print(first + ' ' + second)
+# print(first * (cof//2))
+
+# #         0123456789
+# string = 'Python is the BEST!'
+# print(string[-2])
+
+
+# string = 'Some string'
+# print(len(string))
+# print(string[-5])
+# print(string[11])
+
+
+# idx = 6
+# #         0123456789
+# string = 'Some string'
+# print(string[idx:] + string[:idx])
+
+# string = 'Some string'
+# print(string[::-1])
+
+
+# string = 'Some string'
+# print(string)
+# string = string.replace('m', 'M')
+# print(string)
+
+# my_name = 'STONE'
+# my_age = 40
+#
+# string = 'Меня зовут {}. И мне {} лет'
+# print(string.format(my_name, my_age))
+#
+# string = 'Меня зовут {1}. И мне {0} лет'
+# print(string.format(my_age, my_name))
+#
+# string = 'Меня зовут {name}. И мне {age} лет'
+# print(string.format(name=my_name, age=my_age))
+
+# my_name = 'STONE'
+# my_age = 40
+#
+# string = f'Меня зовут {my_name}. И мне {my_age} лет'
+# print(string)
+
+# bool
+# status = True
+# status = False
+
+# print((True or True) and False)
+# print(True or True and False)
+
+# a = True
+# print(a)
+# print(type(a))
+# print(23 + a)
+
+# a = 5
+# b = 3
+# string = 'NYR'
+# print(string[(a > b) + 1])
+
+
+# None
+
+# a = 'str'
+# b = 'st'
+# b += 'r'
+# print(a == b)
+# print(a is b)
+#
+# print(id(a))
+# print(id(b))
+
+a = None
+print(a is None)
