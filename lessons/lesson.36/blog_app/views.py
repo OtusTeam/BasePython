458ef971abd22db580590d514c38b62a7347cd86
Stanislav-Wise <111073899+Stanislav-Wise@users.noreply.github.com>
2025-02-26 23:27:03 +0300

    Add lesson.36: Знакомство с Django

---
+++ lessons/lesson.36/blog_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the blog index.")
+
+
+def about(request):
+    return HttpResponse("About me info")
