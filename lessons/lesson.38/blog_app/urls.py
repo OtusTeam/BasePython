5528907141018d0ae2b162133f210880d259855e
Stanislav-Wise <111073899+Stanislav-Wise@users.noreply.github.com>
2025-03-05 22:18:07 +0300

    Add lesson.28: Django Templates

---
+++ lessons/lesson.38/blog_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('posts/', views.post_list, name='post_list'),
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('authors/', views.author_list, name='author_list'),
+    path('authors/<int:author_id>/', views.author_detail, name='author_detail'),
+]
