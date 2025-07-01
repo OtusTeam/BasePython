078aff1d5558ffcbbfda766f3824862b4ad094d6
Stanislav-Wise <111073899+Stanislav-Wise@users.noreply.github.com>
2025-07-01 22:34:07 +0300

    Add lesson.23: Docker

---
+++ lessons/lesson.23/routers/index_routers.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    title1 = 'Главная страница - работаем в Docker!!!  !!!!kjsdhfksjdhf'
+    text = "Главная страница компании "
+    context = {"request": request,
+               "title": title1,
+               "text": text}
+    return templates.TemplateResponse("index.html", context=context)
+
+
+@router.get("/about/", response_class=HTMLResponse)
+def about(request: Request):
+    title1 = 'О нас'
+    text = "Подробная статья о нас"
+    context = {"request": request,
+               "title": title1,
+               "text": text}
+    return templates.TemplateResponse("about.html", context=context)
