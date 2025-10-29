05c066f172244d77b6c10974f2c0ebed9707dcf3
Stanislav-Wise <111073899+Stanislav-Wise@users.noreply.github.com>
2025-10-29 21:55:16 +0300

    add lesson.44: Django tests

---
+++ lessons/lesson.44/blog_app/tests/test_forms.py
@@ -0,0 +1,66 @@
+import pytest
+from blog_app.models import Post, Author, Tag
+from blog_app.forms import PostForm, PostModelForm
+
+
+@pytest.mark.django_db
+def test_post_form_valid():
+    """Проверяем валидацию PostForm"""
+    form_data = {
+        'title': 'Тестовый пост',
+        'content': 'Это содержание тестового поста',
+        'rating': 10
+    }
+    form = PostForm(data=form_data)
+
+    assert form.is_valid()
+
+    clean_data = form.cleaned_data
+    assert clean_data['title'] == form_data['title']
+
+
+
+
+@pytest.mark.django_db
+def test_postmodelform_valid(author_1):
+    tag = Tag.objects.create(name='python')
+    form_data = {
+        'title': 'Тестовый пост',
+        'content': 'Это содержание тестового поста',
+        'rating': 10,
+        'author': author_1,
+        'tags': [tag.id],
+    }
+
+    form = PostModelForm(data=form_data)
+    assert form.is_valid()
+
+
+@pytest.mark.django_db
+def test_postmodelform_valid_rating_negative(author_1):
+    tag = Tag.objects.create(name='python')
+    form_data = {
+        'title': 'Тестовый пост',
+        'content': 'Это содержание тестового поста',
+        'rating': -5,
+        'author': author_1,
+        'tags': [tag.id],
+    }
+
+    form = PostModelForm(data=form_data)
+    assert not form.is_valid()
+
+
+@pytest.mark.django_db
+def test_postmodelform_valid_title_negative(author_1):
+    tag = Tag.objects.create(name='python')
+    form_data = {
+        'title': 'Тестовый пост крипта',
+        'content': 'Это содержание тестового поста',
+        'rating': 10,
+        'author': author_1,
+        'tags': [tag.id],
+    }
+
+    form = PostModelForm(data=form_data)
+    assert not form.is_valid()
