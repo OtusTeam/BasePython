d8dcd930a878b2cf873962611b05436ca699d93e
Stanislav-Wise <111073899+Stanislav-Wise@users.noreply.github.com>
2025-11-27 20:00:01 +0300

    Delete lessons/lesson.13/lesson.15 directory

--- lessons/lesson.13/lesson.15/step02.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-logging.basicConfig(level=logging.DEBUG)
-logging.debug('Это DEBUG сообщение')
-logging.info('Это информационное сообщение')
-logging.warning('Это WARNING сообщение')
-logging.error('Это EROOR сообщение')
-logging.critical('Это CRITICAL сообщение')
--- lessons/lesson.13/lesson.15/step9.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-my_pattern = r'\d+'
-string = 'Мой номер телефона 123-456-789. Код 0022'
-
-
-result = re.findall(my_pattern, string)
-print(result)
