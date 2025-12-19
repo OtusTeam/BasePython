c188a4e9155b4a8c63f27067c5f55f11f16dc81d
Stanislav-Wise <111073899+Stanislav-Wise@users.noreply.github.com>
2025-12-19 17:50:34 +0300

    Add lesson.39: Django admin

---
+++ lessons/lesson.38/blog_app/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from blog_app.models import Post
+
+
+# Create your views here.
+def index(request):
+    """Главная страница."""
+    context = {
+        'title': 'Главная страница !'
+    }
+    return render(request, 'blog_app/index.html', context=context)
+
+
+def about(request):
+    """Страница о нас."""
+    context = {
+        'title': 'О нас !'
+    }
+    return render(request, 'blog_app/about.html', context=context)
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        'title': 'Список постов',
+        'posts': posts,
+    }
+    return render(request, 'blog_app/post_list.html', context=context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'title': post.title,
+        'post': post,
+    }
+    return render(request, 'blog_app/post_detail.html', context=context)
