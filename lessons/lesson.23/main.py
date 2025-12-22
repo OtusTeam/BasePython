5e3d7a989e3334cde5205547529c003ca807f630
Stanislav-Wise <111073899+Stanislav-Wise@users.noreply.github.com>
2025-12-22 22:46:02 +0300

    Add lesson.23: Docker

---
+++ lessons/lesson.23/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World OTUS????-==-=-=-=-=-'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8000, debug=True)
