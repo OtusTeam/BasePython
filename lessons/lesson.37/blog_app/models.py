27fce9f8ed678b1b304a06e8ab3e7f185adece99
Stanislav-Wise <111073899+Stanislav-Wise@users.noreply.github.com>
2026-02-17 21:59:43 +0300

    Add lesson.37

---
+++ lessons/lesson.37/blog_app/models.py
@@ -0,0 +1,68 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    # author = models.CharField(max_length=100)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='posts')
+    rating = models.IntegerField(default=0)
+    tags = models.ManyToManyField('Tag', related_name='posts')
+
+    # class Meta:
+    #     db_table = 'new_post'
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    # author = models.CharField(max_length=100)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return f'Коммент Автора {self.author}'
+
+    def __repr__(self):
+        return f'Коммент Автора {self.author}'
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+
+class AuthorProfile(models.Model):
+    author = models.OneToOneField(Author, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField()
+    website = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f'Профиль {self.author.name}'
+
+    def __repr__(self):
+        return f'Профиль {self.author.name}'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
