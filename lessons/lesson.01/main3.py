017d71e63b15fc076c40ac1902ca825629f8b6d6
Stanislav-Wise <111073899+Stanislav-Wise@users.noreply.github.com>
2026-03-05 17:47:19 +0300

    Delete lessons/lesson.01 directory

--- lessons/lesson.01/main3.py
+++
@@ -1,9 +0,0 @@
-import keyword
-
-
-# max, min, sum,
-# and, or, not
-# if , else, elif
-#     def
-
-print(keyword.kwlist)
--- lessons/lesson.01/main7.py
+++
@@ -1,5 +0,0 @@
-import random
-
-
-number = random.randint(1, 100)
-print(number)
